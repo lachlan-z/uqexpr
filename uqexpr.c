edb5eafeb6a52da5c19614e3d71b96db24856e0f
s4808239@uq.edu.au <s4808239@uq.edu.au@bbcfff25-ffe9-9844-9f6b-2e31efc2ea72>
2025-03-06 05:12:56 +0000

    Updated --define in command_arg_check to store variable name and value in  astruct. This struct must be changed to be dynamically sized.
    Be aware there are print statements used for debug.

--- uqexpr.c
+++ uqexpr.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char* name;
+    double value;
+} Var;
+
+Var variables[10];
 
 void command_arg_check(int argc, char** argv) {
-    
+     
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "--define") == 0) {
             printf("Includes define\n");
+
+            char* string_parse = strtok(argv[i+1], "=");
+            
+            variables[0].name = malloc(strlen(string_parse)+1);
+            strcpy(variables[0].name, string_parse);
+
+            variables[0].value = atof(strtok(NULL, "="));
+            
+            printf("%s = %f", variables[0].name, variables[0].value);
         } else if (strcmp(argv[i], "--forloop") == 0) {
             printf("Includes for loop\n");
         } else if (strcmp(argv[i], "--significantfigs") == 0) {
